9068e195edfaedcdc984a1b1d3d07001d639878f
Luke Sloane-Bulger <luke.sloanebulger@protonmail.com>
2024-06-07 21:17:53 +0100

    main - update

---
+++ next.config.mjs
@@ -0,0 +1,9 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+    trailingSlash: true,
+    reactStrictMode: false,
+    swcMinify: true,
+    output: 'export',
+};
+
+export default nextConfig;
