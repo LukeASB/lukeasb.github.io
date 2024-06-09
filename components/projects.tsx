597735353bcca75adc8ce45d7c400be46f8901db
Luke Sloane-Bulger <luke.sloanebulger@protonmail.com>
2024-06-09 18:51:37 +0100

    main - update

--- components/projects.tsx
+++ components/projects.tsx
@@ -5,7 +5,19 @@ const Project: React.FC = () => {
                 <div className="row text-center">
                     <h1>Projects</h1>
                 </div>
-                <p>Projects...</p>
+                <div className="container">
+                    <div className="row">
+                        <div className="col-md-2 mb-2">
+                            <div className="card">
+                                <img src="https://via.placeholder.com/200" className="card-img-top" alt="Project 1" />
+                                <div className="card-body">
+                                    <h5 className="card-title">Project 1</h5>
+                                    <a href="#" className="btn btn-primary">Read More</a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
             </section>
         </div>
     );
